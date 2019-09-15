76aa333b226f40f00d2fe77ef6c620c66bb74158
Sergey Vaysman <vaysman@live.com>
2019-09-15 23:56:07 +0300

    Security for edit salary method

--- src/main/java/ru/yandex/assignment/employeesalary/EmployeeSalaryApplication.java
+++ src/main/java/ru/yandex/assignment/employeesalary/EmployeeSalaryApplication.java
@@ -2,12 +2,39 @@ package ru.yandex.assignment.employeesalary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class EmployeeSalaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeSalaryApplication.class, args);
+	}
+
+	@Configuration
+	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.inMemoryAuthentication().withUser("USER").password("USER").roles("USER");
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.httpBasic()
+					.and()
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/api/edit_employee").hasRole("USER")
+					.and()
+					.csrf().disable()
+					.formLogin().disable();
+		}
 	}
 
 }
