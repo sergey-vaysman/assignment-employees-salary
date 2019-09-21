a75320f33ab9602faf51e2b390279453a813d185
Sergey Vaysman <vaysman@live.com>
2019-09-21 15:20:39 +0300

    front-end resources

---
+++ src/main/resources/static/precache-manifest.4e82f979df9559ab5a93f1ada0407fda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7562bc2e5266981d52ed5e88459312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f66a93c94a1c1dda39d1",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "95cc17faa9b3980a5b27",
+    "url": "/static/css/main.169e690e.chunk.css"
+  },
+  {
+    "revision": "f66a93c94a1c1dda39d1",
+    "url": "/static/js/2.6914ef34.chunk.js"
+  },
+  {
+    "revision": "95cc17faa9b3980a5b27",
+    "url": "/static/js/main.0c2cf8d2.chunk.js"
+  },
+  {
+    "revision": "3a42d77688d13d8836ee",
+    "url": "/static/js/runtime-main.48e5b6a0.js"
+  }
+]);
